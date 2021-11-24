9fc3b148ac758604fe70963f8bafe1923241ca5d
StefanGreve <greve.stefan@outlook.jp>
2021-11-24 23:00:41 +0100

    Init commit

---
+++ LibraryManagementSystem/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using LibraryManagementSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LibraryManagementSystem
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ LibraryManagementSystem/ViewModels/ShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Caliburn.Micro;
+
+namespace LibraryManagementSystem.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private string _firstName = "Stefan Greve";
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+    }
+}
